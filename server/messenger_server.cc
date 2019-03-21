259d060b45c6c3a6e6b78bd23599991125c33992
Priyanka Patil <pripatil@uw.edu>
2019-03-21 15:05:51 -0700

    server code added

--- server/messenger_server.cc
+++ server/messenger_server.cc
@@ -45,10 +45,10 @@ int main(void)
 
         std::cout << "Got new send request " << request.dump() << std::endl;  
 
-        messeges[next_id] = std::make_pair(
+        messeges[next_id] = std::make_tuple(
           unix_timestamp(),
-          request["user"] = std::to_string(request["user"]),
-          request["message"] = std::to_string(request["message"])
+          request["user"],
+          request["message"]
         );
 
         result["result"] = "ok";
@@ -57,24 +57,31 @@ int main(void)
 
     });
 
-    svr.Get(R"/receive", [&](const Request& req, Response& res) {
-        std::cout << "Received a message from = " << req.matches[1] << "\n";
-        auto id = std::stoi(req.matches[1].str());
-        json result;
-        if ( messeges.find(id) != messeges.end() ) {
-            result = { 
-                { "result", "ok" },
-                { "id", id },
-                { "timestamp", std::get<0>(messeges[id]) },
-                { "x", std::get<1>(messeges[id]) },
-                { "y", std::get<2>(messeges[id]) },
-                { "temperature", std::get<3>(messeges[id]) }                                      
-            };
-        } else {
+    svr.Get("/receive", [&](const Request& req, Response& res) {
+
+        //std::cout << "Received a message from = " /*<< req.matches[1]*/ << "\n";
+        //auto id = std::stoi(req.matches[1].str());
+        json result, request;
+
+        try {
+            request = json::parse(req.body);
+        } catch(json::exception e) {
             result["result"] = "error";
-            result["message"] = "not found";
-            res.status = 404;
+            result["message"] = e.what();
+            res.set_content(result.dump(), "json");
+            return;
         }
+
+        std::cout << "New msg received =  " << request.dump() << std::endl;  
+
+        messeges[next_id] = std::make_tuple(
+          unix_timestamp(),
+          request["user"],
+          request["message"]
+        );
+
+        result["result"] = "ok";
+        result["id"] = next_id++;
         res.set_content(result.dump(), "json");
     });
 
--- src/messenger-client.cc
+++ src/messenger-client.cc
@@ -37,9 +37,6 @@ namespace elma {
     Client& Client::post(std::string url, json data, std::function<void(json&)> handler) {
         // TODO: problem 2
         _post_thread(url, data, handler);
-        //std::thread t(&Client::_post_thread,this, url, data, handler);
-        //t.detach(); // detaching means we don't have to join later
-
     }
 
     Client& Client::process_responses() {
@@ -71,19 +68,11 @@ namespace elma {
 
     const std::shared_ptr<httplib::Response> Client::_post_aux(std::string url, json data) {
         auto parts = url_parts(url);
-        // httplib::Params params = data.dump();
-        // std::cout << "Here???..." << parts.second.c_str() << data.dump() << "\n";
         if ( _use_ssl ) {
-            httplib::SSLClient cli(parts.first.c_str(), 443);
-            //return cli.Post(parts.second.c_str(), data.dump().c_str());
-            //std::cout << "Posting secure..." << parts.second.c_str() << data.dump() << "\n";
-            // return cli.Post(parts.second.c_str(), params);               
+            httplib::SSLClient cli(parts.first.c_str(), 443);        
             return cli.Post(parts.second.c_str(), data.dump(), "text/plain");
         } else {
             httplib::Client cli(parts.first.c_str(), 80);
-            //std::cout << "Posting http..." << parts.second.c_str() << data.dump() << "\n";
-            //return cli.Post(parts.second.c_str(), data.dump().c_str());   
-            // return cli.Post(parts.second.c_str(), params); 
             return cli.Post(parts.second.c_str(), data.dump(), "text/plain");          
         }        
     }
@@ -128,7 +117,6 @@ namespace elma {
     void Client::_post_thread(std::string url, json data, std::function<void(json&)> handler) {
 
         json json_response;
-        //std::cout << "I am at line 131" << std::endl;
         try {
             auto response = _post_aux(url, data);
 
